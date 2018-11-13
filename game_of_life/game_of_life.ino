a31faaf0b358214ef49e76fe11debaaa274e9d72
Unknown <sautaxgaming@gmail.com>
2018-11-13 19:56:20 +0100

    just beautify

--- game_of_life/game_of_life.ino
+++ game_of_life/game_of_life.ino
@@ -6,219 +6,270 @@ bool upP = false;
 bool downP = false;
 bool BP = false;
 
-const char* textr = "Randomized !" ;
-const char* textc = "Cleared !" ;
+const char *textr = "Randomized !";
+const char *textc = "Cleared !";
 
-const char* entriesEoff[] = {
-  "Enter Edit mode",
-  "Clear grid",
-  "Randomize grid",
-  "Return",
+const char *entriesEoff[] = {
+    "Enter Edit mode",
+    "Clear grid",
+    "Randomize grid",
+    "Return",
 };
-const char* entriesEon[] = {
-  "Quit Edit mode",
-  "Clear grid",
-  "Randomize grid",
-  "Return",
+const char *entriesEon[] = {
+    "Quit Edit mode",
+    "Clear grid",
+    "Randomize grid",
+    "Return",
 };
 bool Emode = false;
 long fCount = 0;
 
-
 Color colors[10] = {BLACK, BLUE, LIGHTBLUE, LIGHTGREEN, GREEN, BEIGE, YELLOW, ORANGE, RED, WHITE};
 
 bool Play = true;
-uint8_t grid[80][64] ;
-unsigned long time = 0 ;
-uint16_t temp = 0 ;
+uint8_t grid[80][64];
+unsigned long time = 0;
+uint16_t temp = 0;
 uint16_t pointer[2] = {40, 32};
 
-
-
-void setup() {
+void setup()
+{
 
   gb.begin();
-  for (int x = 0; x < 80; x++) {
-    for (int y = 0; y < 64; y++) {
+  for (int x = 0; x < 80; x++)
+  {
+    for (int y = 0; y < 64; y++)
+    {
       grid[x][y] = random(0, 2);
     }
   }
 }
 
-void loop() {
-  while (!gb.update());
+void loop()
+{
+  while (!gb.update())
+    ;
   fCount += 1;
 
-  if (upP && pointer[1] > 0) {
+  if (upP && pointer[1] > 0)
+  {
     pointer[1] -= 1;
   }
-  if (downP && pointer[1] < 63) {
+  if (downP && pointer[1] < 63)
+  {
     pointer[1] += 1;
   }
-  if (leftP && pointer[0] > 0) {
+  if (leftP && pointer[0] > 0)
+  {
     pointer[0] -= 1;
   }
-  if (rightP && pointer[0] < 79) {
+  if (rightP && pointer[0] < 79)
+  {
     pointer[0] += 1;
   }
 
-
-  if (gb.buttons.pressed(BUTTON_B)) {
+  if (gb.buttons.pressed(BUTTON_B))
+  {
     BP = true;
   }
-  if (gb.buttons.released(BUTTON_B)) {
+  if (gb.buttons.released(BUTTON_B))
+  {
     BP = false;
   }
 
-  if (gb.buttons.pressed(BUTTON_DOWN)) {
-    if (Emode && pointer[1] < 63) {
-      if (BP) {
-        downP = true ;
+  if (gb.buttons.pressed(BUTTON_DOWN))
+  {
+    if (Emode && pointer[1] < 63)
+    {
+      if (BP)
+      {
+        downP = true;
       }
-      else {
+      else
+      {
         pointer[1] += 1;
       }
     }
   }
-  if (gb.buttons.pressed(BUTTON_UP)) {
-    if (Emode && pointer[1] > 0) {
-      if (BP) {
+  if (gb.buttons.pressed(BUTTON_UP))
+  {
+    if (Emode && pointer[1] > 0)
+    {
+      if (BP)
+      {
         upP = true;
       }
-      else {
+      else
+      {
         pointer[1] -= 1;
       }
     }
   }
-  if (gb.buttons.pressed(BUTTON_RIGHT)) {
-    if (Emode&& pointer[0] < 79) {
-      if (BP) {
+  if (gb.buttons.pressed(BUTTON_RIGHT))
+  {
+    if (Emode && pointer[0] < 79)
+    {
+      if (BP)
+      {
         rightP = true;
       }
-      else {
+      else
+      {
         pointer[0] += 1;
       }
-
     }
   }
-  if (gb.buttons.pressed(BUTTON_LEFT)) {
-    if (Emode && pointer[0] > 0) {
-      if (BP) {
+  if (gb.buttons.pressed(BUTTON_LEFT))
+  {
+    if (Emode && pointer[0] > 0)
+    {
+      if (BP)
+      {
         leftP = true;
-      } else {
+      }
+      else
+      {
         pointer[0] -= 1;
       }
     }
   }
 
-
-  if (gb.buttons.released(BUTTON_DOWN)) {
-    if (Emode) {
-      downP = false ;
+  if (gb.buttons.released(BUTTON_DOWN))
+  {
+    if (Emode)
+    {
+      downP = false;
     }
   }
-  if (gb.buttons.released(BUTTON_UP)) {
-    if (Emode) {
+  if (gb.buttons.released(BUTTON_UP))
+  {
+    if (Emode)
+    {
       upP = false;
     }
   }
-  if (gb.buttons.released(BUTTON_RIGHT)) {
-    if (Emode) {
+  if (gb.buttons.released(BUTTON_RIGHT))
+  {
+    if (Emode)
+    {
       rightP = false;
     }
   }
-  if (gb.buttons.released(BUTTON_LEFT)) {
-    if (Emode) {
+  if (gb.buttons.released(BUTTON_LEFT))
+  {
+    if (Emode)
+    {
       leftP = false;
     }
   }
 
-
-
-
-  if (gb.buttons.pressed(BUTTON_A)) {
-    if (Emode) {
-      if (grid[pointer[0]][pointer[1]]) {
+  if (gb.buttons.pressed(BUTTON_A))
+  {
+    if (Emode)
+    {
+      if (grid[pointer[0]][pointer[1]])
+      {
         grid[pointer[0]][pointer[1]] = 0;
-      } else {
+      }
+      else
+      {
         grid[pointer[0]][pointer[1]] = 1;
       }
-
-    } else {
-      if (Play) {
-        const char* text = "Paused" ;
-        gb.gui.popup( text,  25);
+    }
+    else
+    {
+      if (Play)
+      {
+        const char *text = "Paused";
+        gb.gui.popup(text, 25);
         Play = false;
-      } else {
-        const char* text = "Playing" ;
-        gb.gui.popup( text,  25);
+      }
+      else
+      {
+        const char *text = "Playing";
+        gb.gui.popup(text, 25);
         Play = true;
       }
     }
-
   }
-  if (gb.buttons.pressed(BUTTON_MENU)) {
+  if (gb.buttons.pressed(BUTTON_MENU))
+  {
 
     uint8_t entry;
-    if (Emode) {
+    if (Emode)
+    {
       entry = gb.gui.menu("--Menu--", entriesEon, 4);
-      if (entry == 0) {
+      if (entry == 0)
+      {
         Emode = 0;
-        const char* text = "Edit mode OFF" ;
-        gb.gui.popup( text,  25);
+        const char *text = "Edit mode OFF";
+        gb.gui.popup(text, 25);
       }
-    } else {
+    }
+    else
+    {
       entry = gb.gui.menu("--Menu--", entriesEoff, 4);
-      if (entry == 0) {
+      if (entry == 0)
+      {
         Emode = 1;
-        const char* text = "Edit mode ON" ;
-        gb.gui.popup( text,  25);
+        const char *text = "Edit mode ON";
+        gb.gui.popup(text, 25);
       }
     }
 
-    switch (entry) {
-      case 1 :
-        for (int x = 0; x < 80; x++) {
-          for (int y = 0; y < 64; y++) {
-            grid[x][y] = 0;
-          }
+    switch (entry)
+    {
+    case 1:
+      for (int x = 0; x < 80; x++)
+      {
+        for (int y = 0; y < 64; y++)
+        {
+          grid[x][y] = 0;
         }
+      }
 
-        gb.gui.popup( textc,  25);
-        break;
+      gb.gui.popup(textc, 25);
+      break;
 
-      case 2 :
-        for (int x = 0; x < 80; x++) {
-          for (int y = 0; y < 64; y++) {
-            grid[x][y] = random(0, 2);
-
-          }
+    case 2:
+      for (int x = 0; x < 80; x++)
+      {
+        for (int y = 0; y < 64; y++)
+        {
+          grid[x][y] = random(0, 2);
         }
+      }
 
-        gb.gui.popup( textr,  25);
-        break;
+      gb.gui.popup(textr, 25);
+      break;
     }
-
   }
   gb.display.clear();
-  if (Play && !Emode) {
+  if (Play && !Emode)
+  {
     updateG();
   }
-  for (int x = 0; x < 80; x++) {
-    for (int y = 0; y < 64; y++) {
+  for (int x = 0; x < 80; x++)
+  {
+    for (int y = 0; y < 64; y++)
+    {
       gb.display.setColor(colors[grid[x][y]]);
       gb.display.drawPixel(x, y);
     }
   }
-  if (Emode) {
+  if (Emode)
+  {
 
     gb.display.setColor(PURPLE);
-    if (fCount % 2) {
+    if (fCount % 2)
+    {
       gb.display.drawPixel(pointer[0] + 1, pointer[1]);
       gb.display.drawPixel(pointer[0], pointer[1] + 1);
       gb.display.drawPixel(pointer[0] - 1, pointer[1]);
       gb.display.drawPixel(pointer[0], pointer[1] - 1);
-    } else {
+    }
+    else
+    {
       gb.display.drawPixel(pointer[0], pointer[1]);
     }
   }
--- game_of_life/update.ino
+++ game_of_life/update.ino
@@ -1,128 +1,194 @@
-void updateG() {
+void updateG()
+{
   uint8_t temp[80][64];
-  uint8_t n ;
-  for (int x = 0; x < 80; x++) {
-    for (int y = 0; y < 64; y++) {
+  uint8_t n;
+  for (int x = 0; x < 80; x++)
+  {
+    for (int y = 0; y < 64; y++)
+    {
       n = neighbours(grid, x, y);
       temp[x][y] = grid[x][y] + 1;
-      if (grid[x][y] > 0) {
-        if (n > 3 || n < 2) {
-          temp[x][y] = 0;
-        }
-      } else {
-        if (n != 3) {
+      if (grid[x][y] > 0)
+      {
+        if (n > 3 || n < 2)
+        {
           temp[x][y] = 0;
         }
       }
-      if (temp[x][y] > 9) {
+      else
+      {
+        if (n != 3)
+        {
+          temp[x][y] = 0;
+        }
+      }
+      if (temp[x][y] > 9)
+      {
         temp[x][y] = 9;
       }
     }
   }
-  for (int x = 0; x < 80; x++) {
-    for (int y = 0; y < 64; y++) {
+  for (int x = 0; x < 80; x++)
+  {
+    for (int y = 0; y < 64; y++)
+    {
       grid[x][y] = temp[x][y];
-
     }
   }
-
-
-
 }
 
-uint8_t neighbours(uint8_t grid[80][64], uint16_t x, uint16_t y) {
+uint8_t neighbours(uint8_t grid[80][64], uint16_t x, uint16_t y)
+{
   uint8_t sum = 0;
   uint16_t w = 80;
   uint16_t h = 64;
 
-  if (x == 0) {
-    if (grid[w - 1][y]) sum += 1;
-    if (y == 0) {
-      if (grid[w - 1][h - 1]) sum += 1;
-    } else {
-      if (grid[w - 1][y - 1]) sum += 1;
+  if (x == 0)
+  {
+    if (grid[w - 1][y])
+      sum += 1;
+    if (y == 0)
+    {
+      if (grid[w - 1][h - 1])
+        sum += 1;
     }
-    if (y == h - 1) {
-      if (grid[w - 1][0]) sum += 1;
-    } else {
-      if (grid[w - 1][y + 1]) sum += 1;
+    else
+    {
+      if (grid[w - 1][y - 1])
+        sum += 1;
     }
-  }
-
-  if (x == w - 1) {
-    if (grid[0][y]) sum += 1;
-    if (y == 0) {
-      if (grid[0][h - 1]) sum += 1;
-    } else {
-      if (grid[0][y - 1]) sum += 1;
+    if (y == h - 1)
+    {
+      if (grid[w - 1][0])
+        sum += 1;
     }
-    if (y == h - 1) {
-      if (grid[0][0]) sum += 1;
-    } else {
-      if (grid[0][y + 1]) sum += 1;
+    else
+    {
+      if (grid[w - 1][y + 1])
+        sum += 1;
     }
   }
 
-  if (y == 0) {
-    if (grid[x][h - 1]) sum += 1;
-    if (x == 0) {
-      if (grid[w - 1][h - 1]) sum += 1;
-    } else {
-      if (grid[x - 1][h - 1]) sum += 1;
+  if (x == w - 1)
+  {
+    if (grid[0][y])
+      sum += 1;
+    if (y == 0)
+    {
+      if (grid[0][h - 1])
+        sum += 1;
     }
-    if (x == w - 1) {
-      if (grid[0][h - 1]) sum += 1;
-    } else {
-      if (grid[x + 1][h - 1]) sum += 1;
+    else
+    {
+      if (grid[0][y - 1])
+        sum += 1;
     }
-  }
-
-  if (y == h - 1) {
-    if (grid[x][0]) sum += 1;
-    if (x == 0) {
-      if (grid[w - 1][0]) sum += 1;
-    } else {
-      if (grid[x - 1][0]) sum += 1;
+    if (y == h - 1)
+    {
+      if (grid[0][0])
+        sum += 1;
     }
-    if (x == w - 1) {
-      if (grid[0][0]) sum += 1;
-    } else {
-      if (grid[x + 1][0]) sum += 1;
+    else
+    {
+      if (grid[0][y + 1])
+        sum += 1;
     }
   }
 
-
-
-
-
-  if (x != 0) {
-    if (grid[x - 1][y]) sum += 1;
-    if (y != h - 1) {
-      if (grid[x - 1][y + 1]) sum += 1;
+  if (y == 0)
+  {
+    if (grid[x][h - 1])
+      sum += 1;
+    if (x == 0)
+    {
+      if (grid[w - 1][h - 1])
+        sum += 1;
+    }
+    else
+    {
+      if (grid[x - 1][h - 1])
+        sum += 1;
+    }
+    if (x == w - 1)
+    {
+      if (grid[0][h - 1])
+        sum += 1;
+    }
+    else
+    {
+      if (grid[x + 1][h - 1])
+        sum += 1;
     }
   }
 
-  if (y != 0) {
-    if (grid[x][y - 1]) sum += 1;
-    if (x != 0) {
-      if (grid[x - 1][y - 1]) sum += 1;
+  if (y == h - 1)
+  {
+    if (grid[x][0])
+      sum += 1;
+    if (x == 0)
+    {
+      if (grid[w - 1][0])
+        sum += 1;
     }
-    if (x != w - 1) {
-      if (grid[x + 1][y - 1]) sum += 1;
+    else
+    {
+      if (grid[x - 1][0])
+        sum += 1;
+    }
+    if (x == w - 1)
+    {
+      if (grid[0][0])
+        sum += 1;
+    }
+    else
+    {
+      if (grid[x + 1][0])
+        sum += 1;
     }
   }
 
-  if (x != w - 1) {
-    if (grid[x + 1][y]) sum += 1;
+  if (x != 0)
+  {
+    if (grid[x - 1][y])
+      sum += 1;
+    if (y != h - 1)
+    {
+      if (grid[x - 1][y + 1])
+        sum += 1;
+    }
   }
 
-  if (y != h - 1) {
-    if (grid[x][y + 1]) sum += 1;
-    if (x != w - 1) {
-      if (grid[x + 1][y + 1]) sum += 1;
+  if (y != 0)
+  {
+    if (grid[x][y - 1])
+      sum += 1;
+    if (x != 0)
+    {
+      if (grid[x - 1][y - 1])
+        sum += 1;
+    }
+    if (x != w - 1)
+    {
+      if (grid[x + 1][y - 1])
+        sum += 1;
+    }
+  }
+
+  if (x != w - 1)
+  {
+    if (grid[x + 1][y])
+      sum += 1;
+  }
+
+  if (y != h - 1)
+  {
+    if (grid[x][y + 1])
+      sum += 1;
+    if (x != w - 1)
+    {
+      if (grid[x + 1][y + 1])
+        sum += 1;
     }
   }
   return sum;
-
 }
-
